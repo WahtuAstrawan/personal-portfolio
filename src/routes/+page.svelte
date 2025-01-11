4518e99abbf795c03fd56e296eb1ebe33594f37c
Wahtu Astrawan <chatt699@gmail.com>
2025-01-11 14:51:52 +0800

    chore: adding cdns and background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="slate font-bold">Welcome to SvelteKit</h1>
+<p class="text-green-400">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
